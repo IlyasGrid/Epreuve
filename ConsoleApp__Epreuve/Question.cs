30cb771ae3d4b7b201e262b97e4b50ce37f4b16e
Admin <Admin@DESKTOP-IOMF4D2>
2022-11-01 11:19:01 +0100

    question done

--- ConsoleApp__Epreuve/Question.cs
+++ ConsoleApp__Epreuve/Question.cs
@@ -11,11 +11,42 @@ namespace ConsoleApp__Epreuve
         string ennonce;
         int noteQuestion;
 
+        public void setEnonce()
+        {
+            this.ennonce = Console.ReadLine();
+        }
+
+        public string showEnonce()
+        {
+            return this.ennonce;
+        }
+
+        public void donnerNote(int note)
+        {
+            this.noteQuestion = note;
+        }
+        public int getNoteQuestion()
+        {
+            return this.noteQuestion;
+        }
     }
+
+
+
 
     class Qcm : Question
     {
         int nombreChoix;
+
+        public void setNbrChoix(int nbrchoix)
+        {
+            this.nombreChoix = nbrchoix;
+        }
+
+        public int getNbrChoix()
+        {
+            return this.nombreChoix;
+        }
 
     }
     class QusetionOuverte : Question
