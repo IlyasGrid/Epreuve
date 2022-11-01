cdede2b4686c257371c4cf7e4bc5b4284b8f41c3
Admin <Admin@DESKTOP-IOMF4D2>
2022-11-01 09:58:00 +0100

    Add project files.

---
+++ ConsoleApp__Epreuve/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp__Epreuve
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+    public class Epreuve
+    {
+        string duree;
+        string matiere;
+        
+        
+    }
+}
